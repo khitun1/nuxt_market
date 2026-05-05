9089e0b3ecf57b9473bda315f21df020855f40bd
Иван Хитун <ivankhitun@MacBook-Air-Ivan.local>
2026-05-05 15:00:18 +0500

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,23 @@
 import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default withNuxt(eslintPluginPrettierRecommended, {
-  rules: {
-    quotes: ["error", "double"],
-    "no-console": "error",
-    curly: "error",
-    semi: ["error", "always"],
-    eqeqeq: "error",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "no-unreachable": "error",
-    "prefer-const": "error",
-    "arrow-body-style": "error",
-    "object-shorthand": "error",
-    "no-constant-condition": ["error", { checkLoops: true }],
-    "vue/multi-word-component-names": "off",
+export default withNuxt([
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      quotes: ["error", "double"],
+      "no-console": "error",
+      curly: "error",
+      semi: ["error", "always"],
+      eqeqeq: "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "no-unreachable": "error",
+      "prefer-const": "error",
+      "arrow-body-style": "error",
+      "object-shorthand": "error",
+      "no-constant-condition": ["error", { checkLoops: true }],
+      "vue/multi-word-component-names": "off",
+    },
   },
-});
+]);
